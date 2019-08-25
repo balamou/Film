6acad97d586cf9fc42f09f7b16cee55a6aa21d97
Michel Balamou <michelbalamou@gmail.com>
2019-08-24 23:21:04 -0400

    Initial commit

---
+++ Film/VideoPlayerController.swift
@@ -0,0 +1,22 @@
+//
+//  VideoPlayerController.swift
+//  Film
+//
+//  Created by Michel Balamou on 2019-08-22.
+//  Copyright © 2019 ElasticPanda. All rights reserved.
+//
+
+import UIKit
+
+class VideoPlayerController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
---
+++ FilmTests/FilmTests.swift
@@ -0,0 +1,34 @@
+//
+//  FilmTests.swift
+//  FilmTests
+//
+//  Created by Michel Balamou on 2019-08-22.
+//  Copyright © 2019 ElasticPanda. All rights reserved.
+//
+
+import XCTest
+@testable import Film
+
+class FilmTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
