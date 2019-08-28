2d4366a2f7fe3d0ac5deaaa7ceea40c502615c8d
Michel Balamou <michelbalamou@gmail.com>
2019-08-28 17:03:12 -0400

    Added buttons to the settings view

--- Film/Main/Tabs/Settings/SettingsView.swift
+++ Film/Main/Tabs/Settings/SettingsView.swift
@@ -16,6 +16,9 @@ class SettingsView: UIView {
     let textFieldColor = #colorLiteral(red: 0.1411764706, green: 0.1411764706, blue: 0.1411764706, alpha: 1) // #242424
     let textFieldFont = FontStandard.helveticaNeue(size: 16.0)
     
+    let buttonsColor =  #colorLiteral(red: 0.3254901961, green: 0.3254901961, blue: 0.3254901961, alpha: 1) // #5363BE
+    let buttonsFont = FontStandard.RobotoBold(size: 15.0)
+   
     
     lazy var navBar: CustomNavigationBar = {
         return CustomNavigationBar(title: "settings".localize())
@@ -106,10 +109,40 @@ class SettingsView: UIView {
         return textField
     }()
     
+    // Buttons
+    
+    lazy var saveButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Save".localize(), for: .normal)
+        button.backgroundColor = buttonsColor
+        button.titleLabel?.font = buttonsFont
+        
+        return button
+    }()
+    
+    lazy var refreshButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Refresh".localize(), for: .normal)
+        button.backgroundColor = buttonsColor
+        button.titleLabel?.font = buttonsFont
+        
+        return button
+    }()
+    
+    lazy var logoutButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Logout".localize(), for: .normal)
+        button.backgroundColor = #colorLiteral(red: 0.8980392157, green: 0.03529411765, blue: 0.07843137255, alpha: 1) // #E50914
+        button.titleLabel?.font = buttonsFont
+        
+        return button
+    }()
+    
     class Constraints {
         
         static let textFieldHeight: CGFloat = 30.0
         static var distanceBetweenFields: CGFloat = 15.0
+        static let buttonsMargin: CGFloat = 20.0
         
         // Labels
         
@@ -160,6 +193,29 @@ class SettingsView: UIView {
             textfield.trailingAnchor.constraint(equalTo: topNeighbour.trailingAnchor).isActive = true
             textfield.heightAnchor.constraint(equalToConstant: textFieldHeight).isActive = true
         }
+        
+        // Buttons
+        
+        static func setSaveButton(_ button: UIButton, _ topNeighbour: UIView, _ parent: UIView) {
+            button.topAnchor.constraint(equalTo: topNeighbour.bottomAnchor, constant: 10.0).isActive = true
+            button.heightAnchor.constraint(equalToConstant: 40.0).isActive = true
+            button.leadingAnchor.constraint(equalTo: parent.leadingAnchor, constant: buttonsMargin).isActive = true
+            button.trailingAnchor.constraint(equalTo: parent.trailingAnchor, constant: -buttonsMargin).isActive = true
+        }
+        
+        static func setLogoutButton(_ button: UIButton, _ parent: UIView) {
+            button.bottomAnchor.constraint(equalTo: parent.safeAreaLayoutGuide.bottomAnchor, constant: -20.0).isActive = true
+            button.heightAnchor.constraint(equalToConstant: 40.0).isActive = true
+            button.leadingAnchor.constraint(equalTo: parent.leadingAnchor, constant: buttonsMargin).isActive = true
+            button.trailingAnchor.constraint(equalTo: parent.trailingAnchor, constant: -buttonsMargin).isActive = true
+        }
+        
+        static func setRefreshButton(_ button: UIButton, _ bottomNeighbour: UIView, _ parent: UIView) {
+            button.bottomAnchor.constraint(equalTo: bottomNeighbour.topAnchor, constant: -10.0).isActive = true
+            button.heightAnchor.constraint(equalToConstant: 40.0).isActive = true
+            button.leadingAnchor.constraint(equalTo: parent.leadingAnchor, constant: buttonsMargin).isActive = true
+            button.trailingAnchor.constraint(equalTo: parent.trailingAnchor, constant: -buttonsMargin).isActive = true
+        }
     }
     
     override init(frame: CGRect) {
@@ -178,6 +234,10 @@ class SettingsView: UIView {
         addSubviewLayout(ipAddressField)
         addSubviewLayout(portField)
         
+        addSubviewLayout(saveButton)
+        addSubviewLayout(refreshButton)
+        addSubviewLayout(logoutButton)
+        
         navBar.setConstraints(parent: self)
         Constraints.setUsernameLabel(usernameLabel, self, usernameField)
         Constraints.setLanguageLabel(languageLabel, self, languageField)
@@ -188,6 +248,10 @@ class SettingsView: UIView {
         Constraints.setLanguageTextField(languageField, usernameField)
         Constraints.setIPAddressTextField(ipAddressField, languageField)
         Constraints.setPortTextField(portField, ipAddressField)
+        
+        Constraints.setSaveButton(saveButton, portField, self)
+        Constraints.setLogoutButton(logoutButton, self)
+        Constraints.setRefreshButton(refreshButton, logoutButton, self)
     }
     
     required init?(coder aDecoder: NSCoder) {
