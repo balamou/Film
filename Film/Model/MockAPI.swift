eb1f0b5dc6cf453e2b3915c1a74e933511f6ca68
Michel Balamou <michelbalamou@gmail.com>
2019-08-29 18:31:03 -0400

    Internationalized the app title. Added more words to the localization

---
+++ Film/Model/MockAPI.swift
@@ -0,0 +1,18 @@
+//
+//  MockAPI.swift
+//  Film
+//
+//  Created by Michel Balamou on 2019-08-29.
+//  Copyright © 2019 ElasticPanda. All rights reserved.
+//
+
+import Foundation
+
+
+protocol RequestAPI {
+    
+}
+
+class MockAPI: RequestAPI {
+    
+}
