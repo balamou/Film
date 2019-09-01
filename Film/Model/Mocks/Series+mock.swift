69d43b840a136a755b16774f15e4459840f762ef
Michel Balamou <michelbalamou@gmail.com>
2019-09-01 12:29:41 -0400

    Moved mock data (such as video URLs and Poster URLs) into a MockData class

--- Film/Model/Mocks/Series+mock.swift
+++ Film/Model/Mocks/Series+mock.swift
@@ -12,7 +12,7 @@ import Foundation
 extension Series {
     
     static func getMock() -> Series {
-        let url = "http://192.168.72.46:9989/EN/series/rick_and_morty/S1/E01.mp4"
+        let url = MockData.videoURLs[0]
         let episodes = [1, 2, 3, 4, 5].map { i in Episode(id: i, episodeNumber: i, seasonNumber: 1, videoURL: url) }
         let desc = "An animated series on adult-swim about the infinite adventures of Rick, a genius alcoholic and careless scientist, with his grandson Morty, a 14 year-old anxious boy who is not so smart, but always tries to lead his grandfather with his own morale compass. Together, they explore the infinite universes; causing mayhem and running into trouble."
         
@@ -23,7 +23,7 @@ extension Series {
 extension Episode {
     
     static func getMock() -> Episode {
-        return Episode(id: 1, episodeNumber: 1, seasonNumber: 2, videoURL: "http://192.168.72.46:9989/EN/series/rick_and_morty/S1/E01.mp4")
+        return Episode(id: 1, episodeNumber: 1, seasonNumber: 2, videoURL: MockData.videoURLs[0])
     }
     
 }
