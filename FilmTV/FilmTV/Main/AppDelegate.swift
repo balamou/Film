0b12eda06404ce7de71333acf9b78bcb9c216ee1
Michel Balamou <michelbalamou@gmail.com>
2020-01-23 17:47:28 -0500

    Removed storyboard and added programmatically viewcontroller

--- FilmTV/FilmTV/Main/AppDelegate.swift
+++ FilmTV/FilmTV/Main/AppDelegate.swift
@@ -12,10 +12,18 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    private var coordinator: Coordinator?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        coordinator = Coordinator()
+        self.window = window
+        
+        window.rootViewController = coordinator?.start()
+        window.makeKeyAndVisible()
+        
         return true
     }
 
---
+++ FilmTV/FilmTV/Main/Coordinator.swift
@@ -0,0 +1,17 @@
+//
+//  Coordinator.swift
+//  FilmTV
+//
+//  Created by Michel Balamou on 2020-01-23.
+//  Copyright © 2020 Michel Balamou. All rights reserved.
+//
+
+import UIKit
+
+class Coordinator {
+    
+    func start() -> UIViewController {
+        return ViewController()
+    }
+    
+}
